83b1f7ce0fedf1bdb6b3b12072e1610376821a04
Atiwat Bank Promarin <ap@manaosoftware.com>
2023-02-01 17:42:28 +0700

    Initial Commit

---
+++ marco/marcoApp.swift
@@ -0,0 +1,17 @@
+//
+//  marcoApp.swift
+//  marco
+//
+//  Created by ap on 1/2/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct marcoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
